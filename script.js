97a2c43deb1ef95d893a05754ecf0dbd4a054e45
Aditya Verma <adityaverma1001@gmail.com>
2017-07-12 15:02:45 -0400

    Added continous click draw functionality

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 /*$(document).ready(function(){
-
     for(var i=0; i<16; i++){
-	
 	for(var j=0; j<16; j++){
 	    console.log('o');
 	    $("<div class='square'></div>").appendTo('body');
-
 	}}
      $('.square').css({'width': 100px, 'height': 100px});
-	
-
 });*/
 $(document).ready(function(){
-
+    var contclick=0;
     for(var i=0; i<16; i++){
 	
 	for(var j=0; j<16; j++){
@@ -23,11 +18,18 @@ $(document).ready(function(){
 //	$(".container").append("</br>");
 }
 
-    $(".square").hover(function(){
-	$(this).addClass("on");
+    $(".container").mousedown(function(){
+	contclick=1;
+//	$(this).addClass("on");
 //	$(".on").css("background-color","orange");
-  });
-
+  }).mouseup(function(){
+	contclick=0;
+    });
+        $(".square").mouseout(function(){
+	    if(contclick==1)
+		$(this).addClass("on");
+	});
 });
 
 
+			     
