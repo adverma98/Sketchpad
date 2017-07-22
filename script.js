840a3cd61cacadf437aa7872b987f89dd4372a2f
Aditya Verma <av558@scarletmail.rutgers.edu>
2017-07-22 02:23:07 -0400

    colour not working in chrome

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function colourGrid(){
     $(".square").mouseout(function(){
 	if(contclick==1){
     if(getId('eraserRadio').checked)
-	color="#6db4e9e6";
+	color="#62A2D2";
     else if(getId('redRadio').checked)
 	color='red';
     else if(getId('blueRadio').checked)
